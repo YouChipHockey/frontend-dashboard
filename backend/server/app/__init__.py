2cb40a851097e3f2cf8777362f5af4fee1c11cad
Волощук Вадим Игоревич <kingofsweetsx2k@gmail.com>
2024-07-20 14:58:36 +0300

    let's go

--- backend/server/app/__init__.py
+++ backend/server/app/__init__.py
@@ -5,6 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://yc_admin:00000000@5.182.87.23/YC'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://youchip_admin:00000000@77.221.156.184/yochip'
 db = SQLAlchemy(app) 
 from app import endpoints
